564b662a611be1d012399f760e5b0b18c22bac45
unknown <ginger1881@hotmail.com>
2025-05-13 18:24:06 -0500

    commit bases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/shared/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Ginger';
+  surName= 'Romo'
 }
